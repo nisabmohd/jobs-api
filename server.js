b0f0695022427f0c861aca6e2146fdf0f02100b0
nisabmohd <nisabmohd@gmail.com>
2022-07-05 19:57:46 +0530

    server commit

--- server.js
+++ server.js
@@ -15,20 +15,22 @@ app.use(express.json())
 
 app.get('/all', async (req, res) => {
     const datatimestamp = await jobmodel.findOne({})
-    const nowtime = Date.now()
-    const diff = (nowtime - datatimestamp?.timestamp) / (1000 * 60 * 60);
-    if (!diff) {
+    var diff = 0;
+    if (datatimestamp!=undefined) {
+        const nowtime = Date.now()
+        diff = (nowtime - Date.parse(datatimestamp.timestamp)) / (1000 * 60 * 60);
+    }else{
         const data = await all('')
         await jobmodel.insertMany(data)
-        return res.send(await jobmodel.find({},{timestamp:0}))
+        return res.send(await jobmodel.find({}, { timestamp: 0 }))
     }
     if (diff > 0.55) {
         const data = await all('')
         await jobmodel.deleteMany({})
         await jobmodel.insertMany(data)
-       return res.send(await jobmodel.find({},{timestamp:0}))
+        return res.send(await jobmodel.find({}, { timestamp: 0 }))
     }
-    const oldjobs = await jobmodel.find({},{timestamp:0})
+    const oldjobs = await jobmodel.find({}, { timestamp: 0 })
     res.send(oldjobs)
 })
 
